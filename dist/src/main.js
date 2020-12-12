22d4682e47a13e5255fdda340099ba6fe3d9376b
Master Chief <16513382+117@users.noreply.github.com>
2020-12-12 10:44:07 -0700

    added help command aliases

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,5 +1,3 @@
-#!/bin/sh 
-':' //# comment; exec /usr/bin/env node --experimental-top-level-await --no-warnings --experimental-json-modules --experimental-import-meta-resolve "$0" "$@"
 import { Terminal } from './terminal.js';
 // forever
 new Terminal().loop();
