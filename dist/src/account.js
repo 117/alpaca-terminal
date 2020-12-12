3595c2feb68c5cc7397734385baea3bfd430571f
Master Chief <16513382+117@users.noreply.github.com>
2020-12-11 20:44:58 -0700

    account command is functional

--- dist/src/account.js
+++ dist/src/account.js
@@ -1,8 +1,22 @@
+import _ from 'lodash';
+import { client } from './authenticate.js';
 export default {
     aliases: ['account', 'a'],
     usage: '[field]',
     description: 'view account',
     execute: async (args) => {
-        console.log('account');
+        let account = await client.getAccount();
+        if (args.length == 1) {
+            if (!(args[0] in account)) {
+                throw new Error('field not present in account');
+            }
+            console.log(account[args[0]]);
+            return;
+        }
+        for (let [key, value] of Object.entries(account)) {
+            if (!_.isFunction(value)) {
+                console.log(key.padEnd(24), value);
+            }
+        }
     },
 };
